24d427a4cca20d6960751183e313ecdc328a319c
hemontakhan <76201341+hemontakhan@users.noreply.github.com>
2021-06-04 21:36:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import TransactionScreen from './screens/BookTransactionScreen';
+import SearchScreen from './screens/SearchScreen';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <View>
+         <AppContainer />
+         <Image source={"./assets/camera.jpg"}/>
+        </View>
+      
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: {screen: TransactionScreen},
+  Search: {screen: SearchScreen},
+});
+
+
+const AppContainer =  createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ SearchScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View,StyleSheet } from 'react-native';
+
+export default class Searchscreen extends React.Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text>Search</Text>
+        </View>
+      );
+    }
+  }
+
+  const styles=StyleSheet.create({
+    container:{
+      flex: 1, 
+      justifyContent: 'center', 
+      alignItems: 'center'
+    }
+  })
